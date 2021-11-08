d006dd15578d4da05faf44337418f217e9015370
Afiz Savage <savageafiz9@gmail.com>
2021-11-08 18:05:31 +0000

    Add mobile menu background images

--- javascript/index.js
+++ javascript/index.js
@@ -5,8 +5,10 @@ const header = document.querySelector('header');
 function checkIfMenuOpen(menu, button) {
   if (menu.classList.contains('show')) {
     button.src = 'assets/ic_cross.svg';
+    document.querySelector('body').classList.add('disScroll');
   } else {
     button.src = 'assets/ic_menu.svg';
+    document.querySelector('body').classList.remove('disScroll');
   }
 }
 
