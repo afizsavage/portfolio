5857ec6a5d67e9b19557e8096813707546f068de
Afiz Savage <savageafiz9@gmail.com>
2021-11-08 19:30:08 +0000

    Add click event listener to list items

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,27 @@
 const menuButton = document.querySelector('nav > img');
 const mobileMenu = document.querySelector('nav > div');
 const header = document.querySelector('header');
+const navLinks = document.querySelector('nav a');
+const body = document.querySelector('body');
 
 function checkIfMenuOpen(menu, button) {
   if (menu.classList.contains('show')) {
     button.src = 'assets/ic_cross.svg';
-    document.querySelector('body').classList.add('disScroll');
+    body.classList.add('disScroll');
+    header.classList.add('mobile');
   } else {
     button.src = 'assets/ic_menu.svg';
-    document.querySelector('body').classList.remove('disScroll');
+    body.classList.remove('disScroll');
+    header.classList.remove('mobile');
   }
 }
 
-menuButton.addEventListener('click', () => {
-  mobileMenu.classList.toggle('show');
-  // menuButton.src = 'assets/ic_cross.svg';
-  header.classList.toggle('mobile');
+navLinks.addEventListener('click', () => {
+  mobileMenu.classList.remove('show');
   checkIfMenuOpen(mobileMenu, menuButton);
 });
 
+menuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('show');
+  checkIfMenuOpen(mobileMenu, menuButton);
+});
